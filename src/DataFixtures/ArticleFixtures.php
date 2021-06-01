96f65b1d187417ebe82ebe2f1dd5c14a7d3a2bd0
ludovicjj <jahanlud@gmail.com>
2021-06-01 17:46:29 +0200

    init article fixtures

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,19 +6,49 @@ use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use DateTimeImmutable;
+use DateTime;
+use Nelmio\Alice\Loader\NativeLoader;
+use Nelmio\Alice\Throwable\LoadingThrowable;
 
-class AppFixtures extends Fixture
+class ArticleFixtures extends Fixture
 {
+    const DATA_ENTRY_POINT = __DIR__.'/data/articles.yml';
+
     public function load(ObjectManager $manager)
     {
-        $article = new Article();
-        $article->setTitle('Article 2')
-            ->setSlug('article-2')
-            ->setContent('Hello world')
-            ->setCreatedAt(new DateTimeImmutable('2021-05-01T11:27:00'))
-            ->setIsPublished(false);
-        $manager->persist($article);
+        try {
+            $objectSet = $this->getNativeLoader()->loadFile(self::DATA_ENTRY_POINT);
+            /** @var Article $object */
+            foreach ($objectSet->getObjects() as $object) {
+                [
+                    'dateObject' => $dateObject,
+                    'dateString' => $dateString
+                ] = $this->generateRandomDateBetweenRange('01-01-2020', '01-06-2021');
+                $object->setSlug($object->getSlug() . "-{$dateString}");
+                $object->setCreatedAt($dateObject);
+                $manager->persist($object);
+            }
+        } catch (LoadingThrowable $e) {
+        }
 
         $manager->flush();
+    }
+
+    private function generateRandomDateBetweenRange(string $start, string $end): array
+    {
+        $startTimestamp = DateTime::createFromFormat('d-m-Y', $start)->getTimestamp();
+        $endTimestamp = DateTime::createFromFormat('d-m-Y', $end)->getTimestamp();
+        $randomTimestamp = mt_rand($startTimestamp, $endTimestamp);
+        $dateTimeImmutable = (new DateTimeImmutable())->setTimestamp($randomTimestamp);
+
+        return [
+            'dateObject' => $dateTimeImmutable,
+            'dateString' => $dateTimeImmutable->format('d-m-Y')
+        ];
+    }
+
+    private function getNativeLoader()
+    {
+        return new NativeLoader();
     }
 }
