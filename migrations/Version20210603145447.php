859b188ab1ca2be146f2783728845842b0b00ec0
ludovicjj <jahanlud@gmail.com>
2021-06-03 16:57:15 +0200

    Picture migration

---
+++ migrations/Version20210603145447.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210603145447 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Picture migration. Relation OneToOne with Article';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL, picture_path VARCHAR(255) NOT NULL, picture_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_16DB4F897294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F897294869C FOREIGN KEY (article_id) REFERENCES articles (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE picture');
+    }
+}
