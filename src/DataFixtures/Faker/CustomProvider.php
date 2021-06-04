fae55164374fb353252e4b9271bb67c934a14b42
ludovicjj <jahanlud@gmail.com>
2021-06-04 18:17:28 +0200

    Create png with random color

--- src/DataFixtures/Faker/CustomProvider.php
+++ src/DataFixtures/Faker/CustomProvider.php
@@ -16,13 +16,37 @@ class CustomProvider extends BaseProvider
         parent::__construct($generator);
     }
 
+    /**
+     * Return a random character name
+     *
+     * @return string
+     */
     public function customName(): string
     {
         $key = array_rand($this->names);
         return $this->names[$key];
     }
 
-    public function customImage(string $name): UploadedFile
+    /**
+     * Return a random color
+     *
+     * @return string
+     */
+    public function customColor(): string
+    {
+        $key = array_rand($this->color);
+        return $this->color[$key];
+    }
+
+    /**
+     * Create and save a PNG image into DataPicture directory.
+     * Return an UploadedFile from PNG image.
+     *
+     * @param string $name
+     * @param string $color
+     * @return UploadedFile
+     */
+    public function customImage(string $name, string $color): UploadedFile
     {
         if (!file_exists(self::PATH_DATA_PICTURE)) {
             mkdir(self::PATH_DATA_PICTURE);
@@ -34,15 +58,41 @@ class CustomProvider extends BaseProvider
             throw new HttpException(400, "Cannot Initialize new GD image stream");
         }
 
-        $colorId = imagecolorallocate($imageId, 255, 128, 0);
-
-        if ($colorId === false) {
-            throw new HttpException(400, "Color allocation failed");
-        }
+        $this->setColorAllocation($color, $imageId);
 
         $filename = self::PATH_DATA_PICTURE . '/' . $name . '.png';
         imagepng($imageId, $filename);
         return new UploadedFile($filename, $name, null, null, true);
+    }
+
+    /**
+     * @param string $color
+     * @param resource $imageId
+     */
+    private function setColorAllocation(string $color, $imageId): void
+    {
+        switch ($color) {
+            case 'orange':
+                imagecolorallocate($imageId, 255, 128, 0);
+                break;
+            case 'blue':
+                imagecolorallocate($imageId, 0, 128, 255);
+                break;
+            case 'lightblue':
+                imagecolorallocate($imageId, 156, 227, 254);
+                break;
+            case 'black':
+                imagecolorallocate($imageId, 0, 0, 0);
+                break;
+            case 'yellow':
+                imagecolorallocate($imageId, 240, 247, 9);
+                break;
+            case 'green':
+                imagecolorallocate($imageId, 1, 126, 11);
+                break;
+            default:
+                imagecolorallocate($imageId, 255, 255, 255);
+        }
     }
 
     /** @var string[]  */
@@ -55,5 +105,15 @@ class CustomProvider extends BaseProvider
         'Lara Croft',
         'Zelda',
         'Pac-Man',
+    ];
+
+    /** @var string[]  */
+    private $color = [
+        'orange',
+        'blue',
+        'lightblue',
+        'black',
+        'yellow',
+        'green',
     ];
 }
