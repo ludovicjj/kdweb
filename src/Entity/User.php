2fcfa7c301461289a32ebb60d50da0fc7b92a716
ludovicjj <jahanlud@gmail.com>
2021-06-08 13:52:23 +0200

    Fix phpstan errors

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="users")
  */
 class User implements UserInterface
 {
@@ -15,16 +16,19 @@ class User implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @var int $id
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @var string $email
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @var array<string> $roles
      */
     private $roles = [];
 
@@ -73,6 +77,12 @@ class User implements UserInterface
         return array_unique($roles);
     }
 
+    /**
+     * Set user roles
+     *
+     * @param array<string> $roles
+     * @return $this
+     */
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
@@ -109,7 +119,7 @@ class User implements UserInterface
     /**
      * @see UserInterface
      */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
