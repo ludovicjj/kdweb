75736876f55de25b905e9c80fb2c265598ed01fd
ludovicjj <jahanlud@gmail.com>
2021-06-14 06:30:37 +0200

    Add method createView, return FormView

--- src/HandlerFactory/HandlerInterface.php
+++ src/HandlerFactory/HandlerInterface.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace App\HandlerFactory;
 
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Request;
 
 interface HandlerInterface
 {
-    public function handle(): string;
+    /**
+     * Handle form
+     *
+     * @param Request $request
+     * @param mixed|null $data
+     * @param array $options
+     * @return bool
+     */
+    public function handle(Request $request, $data = null, $options = []): bool;
+
+    /**
+     * @return FormView The view
+     */
+    public function createView(): FormView;
 }
