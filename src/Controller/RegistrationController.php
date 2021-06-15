7333643e5c4e9f3791fd4e669b9ef4e17ff18055
ludovicjj <jahanlud@gmail.com>
2021-06-15 14:59:37 +0200

    Verify User account

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,20 +4,34 @@ namespace App\Controller;
 
 use App\Handler\RegistrationHandler;
 use App\HandlerFactory\HandlerFactory;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use DateTimeImmutable;
 
 class RegistrationController extends AbstractController
 {
     /** @var HandlerFactory $handlerFactory */
     private $handlerFactory;
 
+    /** @var EntityManagerInterface $entityManger */
+    private $entityManger;
+
+    /** @var UserRepository $userRepository */
+    private $userRepository;
+
     public function __construct(
-        HandlerFactory $handlerFactory
+        HandlerFactory $handlerFactory,
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository
     ) {
         $this->handlerFactory = $handlerFactory;
+        $this->entityManger = $entityManager;
+        $this->userRepository = $userRepository;
     }
 
     /**
@@ -37,5 +51,45 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $handler->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}/{token}", name="app_verify_account", methods={"GET"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function verifyUserAccount(Request $request): Response
+    {
+        $user = $this->userRepository->findOneBy(['id' => $request->attributes->get('id')]);
+
+        if ($user === null) {
+            throw new AccessDeniedException();
+        }
+
+        if (
+            $user->getRegistrationToken() === null ||
+            $user->getRegistrationToken() !== $request->attributes->get('token') ||
+            $this->isVerifiedBeforeEndTime($user->getAccountMustBeVerifiedBefore())
+        ) {
+            throw new AccessDeniedException();
+        }
+
+        $user
+            ->setIsVerified(true)
+            ->setAccountVerifiedAt(new DateTimeImmutable('now'))
+            ->setRegistrationToken(null)
+        ;
+
+        $this->entityManger->flush();
+        $this->addFlash("success", "Votre compte est dés à présent activé. Vous pouvez vous authentifié");
+
+        return $this->redirect('app_login');
+    }
+
+    private function isVerifiedBeforeEndTime(DateTimeImmutable $endDate): bool
+    {
+        $startDate = new DateTimeImmutable('now');
+        return $startDate > $endDate;
     }
 }
