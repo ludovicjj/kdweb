11899c904f4d9eee2eb16b510c9f6f1b5e84d5c5
ludovicjj <jahanlud@gmail.com>
2021-06-17 13:13:56 +0200

    Fix phpstan errors

--- src/EventSubscriber/AuthenticationSubscriber.php
+++ src/EventSubscriber/AuthenticationSubscriber.php
@@ -2,9 +2,15 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\AuthenticationEvents;
 use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
+use Symfony\Component\Security\Core\Event\AuthenticationSuccessEvent;
 use Symfony\Component\Security\Http\Event\DeauthenticatedEvent;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
@@ -13,7 +19,25 @@ use Symfony\Component\Security\Http\SecurityEvents;
 
 class AuthenticationSubscriber implements EventSubscriberInterface
 {
-    public static function getSubscribedEvents()
+    /** @var LoggerInterface $securityLogger */
+    private $securityLogger;
+
+    /** @var RequestStack $request */
+    private $request;
+
+    public function __construct(
+        LoggerInterface $securityLogger,
+        RequestStack $request
+    ) {
+        $this->securityLogger = $securityLogger;
+        $this->request = $request;
+
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getSubscribedEvents(): array
     {
         return [
             AuthenticationEvents::AUTHENTICATION_FAILURE => 'onSecurityAuthenticationFailure',
@@ -27,22 +51,63 @@ class AuthenticationSubscriber implements EventSubscriberInterface
 
     public function onSecurityAuthenticationFailure(AuthenticationFailureEvent $event): void
     {
-        // ...
+        ['user_ip' => $userIP] = $this->getRouteNameAndUserIP();
+        $securityToken = $event->getAuthenticationToken();
+        ['email' => $emailEntered] = $securityToken->getCredentials();
+
+        $this->securityLogger->info(
+            sprintf(
+                "Anonymous user with IP: '%s' fail authentication with the following email : '%s'",
+                $userIP,
+                $emailEntered
+            )
+        );
     }
 
-    public function onSecurityAuthenticationSuccess(AuthenticationFailureEvent $event): void
+    public function onSecurityAuthenticationSuccess(AuthenticationSuccessEvent $event): void
     {
-        // ...
+        [
+            'user_ip' => $userIP,
+            'route_name' => $routeName
+        ] = $this->getRouteNameAndUserIP();
+
+        if (empty($event->getAuthenticationToken()->getRoleNames())) {
+            $this->securityLogger->info(
+                sprintf("Anonymous user join us with IP : '%s' on route : '%s'.", $userIP, $routeName)
+            );
+        } else {
+            $userEmail = $this->getUserEmail($event->getAuthenticationToken());
+            $this->securityLogger->info(
+                sprintf("User '%s' authenticated with success. IP: '%s'", $userEmail, $userIP)
+            );
+        }
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event): void
     {
-        // ...
+        $userEmail = $this->getUserEmail($event->getAuthenticationToken());
+        $this->securityLogger->info(
+            sprintf("Welcome '%s' !", $userEmail)
+        );
     }
 
     public function onSecurityLogoutEvent(LogoutEvent $event): void
     {
-        // ...
+        /** @var RedirectResponse|null $response */
+        $response = $event->getResponse();
+
+        /** @var  TokenInterface|null $token */
+        $token = $event->getToken();
+
+        if (!$response || !$token) {
+            return;
+        }
+
+        $userEmail = $this->getUserEmail($token);
+        $targetUrl = $response->getTargetUrl();
+        $this->securityLogger->info(
+            sprintf("'%s' disconnected and redirect to the following url : '%s'", $userEmail, $targetUrl)
+        );
     }
 
     public function onSecurityLogoutOnChange(DeauthenticatedEvent $event): void
@@ -53,5 +118,33 @@ class AuthenticationSubscriber implements EventSubscriberInterface
     public function onSecuritySwitchUser(SwitchUserEvent $event): void
     {
         // ...
+    }
+
+    /**
+     * Return user IP and the current route name
+     *
+     * @return array{user_ip: string, route_name: mixed|null}
+     */
+    private function getRouteNameAndUserIP(): array
+    {
+        $request = $this->request->getCurrentRequest();
+        if (!$request) {
+            return [
+                'user_ip'    => 'unknown',
+                'route_name' => 'unknown'
+            ];
+        }
+
+        return [
+            'user_ip'    => $request->getClientIp() ?? 'unknown',
+            'route_name' => $request->attributes->get('_route')
+        ];
+    }
+
+    private function getUserEmail(TokenInterface $securityToken): string
+    {
+        /** @var User $user */
+        $user = $securityToken->getUser();
+        return $user->getUsername();
     }
 }
