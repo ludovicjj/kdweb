b606379817a552c8f3af7073b74ce3b86172ebe4
ludovicjj <jahanlud@gmail.com>
2021-07-01 11:45:45 +0200

    Type properties, fix getter & setter, update table name

--- src/Entity/AuthLog.php
+++ src/Entity/AuthLog.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\AuthLogRepository;
 use Doctrine\ORM\Mapping as ORM;
+use DateTimeImmutable;
 
 /**
  * @ORM\Entity(repositoryClass=AuthLogRepository::class)
+ * @ORM\Table(name="auth_logs")
  */
 class AuthLog
 {
@@ -14,60 +16,79 @@ class AuthLog
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @var int $id
      */
     private $id;
 
     /**
      * @ORM\Column(type="datetime_immutable")
+     * @var DateTimeImmutable $authAttemptAt
      */
     private $authAttemptAt;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null $userIp
      */
     private $userIp;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @var string $emailEntered
      */
     private $emailEntered;
 
     /**
      * @ORM\Column(type="boolean")
+     * @var boolean $isSuccessfulAuth
      */
     private $isSuccessfulAuth;
 
     /**
      * @ORM\Column(type="datetime_immutable", nullable=true)
+     * @var DateTimeImmutable|null $startBlackListingAt
      */
     private $startBlackListingAt;
 
     /**
      * @ORM\Column(type="datetime_immutable", nullable=true)
+     * @var DateTimeImmutable|null $endBlackListingAt
      */
     private $endBlackListingAt;
 
     /**
      * @ORM\Column(type="boolean")
+     * @var boolean $isRememberMeAuth
      */
     private $isRememberMeAuth;
 
     /**
      * @ORM\Column(type="datetime_immutable", nullable=true)
+     * @var DateTimeImmutable|null $deauthenticatedAt
      */
     private $deauthenticatedAt;
+
+    public function __construct(
+        string $emailEntered,
+        ?string $userIp
+    ) {
+        $this->authAttemptAt = new DateTimeImmutable("now");
+        $this->emailEntered = $emailEntered;
+        $this->userIp = $userIp;
+        $this->isRememberMeAuth = false;
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getAuthAttemptAt(): ?\DateTimeImmutable
+    public function getAuthAttemptAt(): DateTimeImmutable
     {
         return $this->authAttemptAt;
     }
 
-    public function setAuthAttemptAt(\DateTimeImmutable $authAttemptAt): self
+    public function setAuthAttemptAt(DateTimeImmutable $authAttemptAt): self
     {
         $this->authAttemptAt = $authAttemptAt;
 
@@ -86,7 +107,7 @@ class AuthLog
         return $this;
     }
 
-    public function getEmailEntered(): ?string
+    public function getEmailEntered(): string
     {
         return $this->emailEntered;
     }
@@ -98,7 +119,7 @@ class AuthLog
         return $this;
     }
 
-    public function getIsSuccessfulAuth(): ?bool
+    public function getIsSuccessfulAuth(): bool
     {
         return $this->isSuccessfulAuth;
     }
@@ -110,31 +131,31 @@ class AuthLog
         return $this;
     }
 
-    public function getStartBlackListingAt(): ?\DateTimeImmutable
+    public function getStartBlackListingAt(): ?DateTimeImmutable
     {
         return $this->startBlackListingAt;
     }
 
-    public function setStartBlackListingAt(?\DateTimeImmutable $startBlackListingAt): self
+    public function setStartBlackListingAt(DateTimeImmutable $startBlackListingAt): self
     {
         $this->startBlackListingAt = $startBlackListingAt;
 
         return $this;
     }
 
-    public function getEndBlackListingAt(): ?\DateTimeImmutable
+    public function getEndBlackListingAt(): ?DateTimeImmutable
     {
         return $this->endBlackListingAt;
     }
 
-    public function setEndBlackListingAt(?\DateTimeImmutable $endBlackListingAt): self
+    public function setEndBlackListingAt(DateTimeImmutable $endBlackListingAt): self
     {
         $this->endBlackListingAt = $endBlackListingAt;
 
         return $this;
     }
 
-    public function getIsRememberMeAuth(): ?bool
+    public function getIsRememberMeAuth(): bool
     {
         return $this->isRememberMeAuth;
     }
@@ -146,12 +167,12 @@ class AuthLog
         return $this;
     }
 
-    public function getDeauthenticatedAt(): ?\DateTimeImmutable
+    public function getDeauthenticatedAt(): ?DateTimeImmutable
     {
         return $this->deauthenticatedAt;
     }
 
-    public function setDeauthenticatedAt(?\DateTimeImmutable $deauthenticatedAt): self
+    public function setDeauthenticatedAt(DateTimeImmutable $deauthenticatedAt): self
     {
         $this->deauthenticatedAt = $deauthenticatedAt;
 
