ed47d24a733a1599f23b7d36a180294859368dd8
ludovicjj <jahanlud@gmail.com>
2021-07-05 13:50:12 +0200

    Create table rememberme_token

--- src/Command/FixturesReloadCommand.php
+++ src/Command/FixturesReloadCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use LogicException;
 use Symfony\Component\Console\Command\Command;
@@ -21,6 +22,18 @@ class FixturesReloadCommand extends Command
 
     /** @var SymfonyStyle $io */
     private $io;
+
+    /** @var EntityManagerInterface $entityManager */
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        string $name = null
+    )
+    {
+        $this->entityManager = $entityManager;
+        parent::__construct($name);
+    }
 
     protected function configure(): void
     {
@@ -72,6 +85,7 @@ class FixturesReloadCommand extends Command
         $this->runSymfonyCommand('doctrine:database:create', ["--if-not-exists" => true]);
         $this->runSymfonyCommand('doctrine:migrations:migrate', ["--no-interaction" => true]);
         $this->runSymfonyCommand('doctrine:fixtures:load', ["--no-interaction" => true], $arguments);
+        $this->createRememberMeTokenTable();
 
         $this->io->success('Recreate database with schema and load fixtures with success');
 
@@ -106,6 +120,22 @@ class FixturesReloadCommand extends Command
             $application->run(new ArrayInput($options));
         } catch (Exception $exception) {
             throw new LogicException(sprintf("Command %s fail", $command));
+        }
+    }
+
+    private function createRememberMeTokenTable(): void
+    {
+        $sql = "CREATE TABLE `rememberme_token` (
+            `series`   char(88)     UNIQUE PRIMARY KEY NOT NULL,
+            `value`    varchar(88)  NOT NULL,
+            `lastUsed` datetime     NOT NULL,
+            `class`    varchar(100) NOT NULL,
+            `username` varchar(200) NOT NULL
+        );";
+
+        try {
+            $this->entityManager->getConnection()->executeStatement($sql);
+        } catch (\Doctrine\DBAL\Exception $e) {
         }
     }
 }
