7429bab02accab294bbe9e1ff09b65f54f9150f9
ludovicjj <jahanlud@gmail.com>
2021-07-06 16:58:08 +0200

    Update RegistrationFormType with RepeatedPasswordType

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,10 @@ namespace App\Form;
 
 use App\DTO\RegistrationDTO;
 use App\Form\FormExtension\HoneyPotType;
+use App\Form\FormExtension\RepeatedPasswordType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -30,34 +29,7 @@ class RegistrationFormType extends AbstractType
                     "autofocus" => true
                 ]
             ])
-            ->add('password', RepeatedType::class, [
-                "type" => PasswordType::class,
-                "invalid_message" => "Les mot de passe ne correspondent pas.",
-                "required" => true,
-                "first_options" => [
-                    "label" => "Mot de passe",
-                    "label_attr" => [
-                        "title" => "Pour des raison de sécurité, votre mot de passe doit contenir au moins 8 caractères 
-                        dont 1 caractère en majuscule, 1 nombre et un caractères special."
-                    ],
-                    "attr" => [
-                        "pattern" => "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*[-+_!@#$%^&*., ?]).{8,}$",
-                        "title" => "Minimum 8 caractères avec au moins une majuscule, un chiffre et un caractère spécial",
-                        "maxlength" => 255
-                    ]
-                ],
-                "second_options" => [
-                    "label" => "Confirmer votre mot de passe",
-                    "label_attr" => [
-                        "title" => "Confirmer votre mot de passe"
-                    ],
-                    "attr" => [
-                        "pattern" => "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*[-+_!@#$%^&*., ?]).{8,}$",
-                        "title" => "Confirmer votre mot de passe",
-                        "maxlength" => 255
-                    ]
-                ]
-            ])
+            ->add('password', RepeatedPasswordType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 "label" => "J'accepte les conditions d'utilisation de se site",
                 'mapped' => false,
