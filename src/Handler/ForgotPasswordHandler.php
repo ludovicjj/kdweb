a929e3d4f24586e88435941f8a8c21914c9933d4
ludovicjj <jahanlud@gmail.com>
2021-07-06 17:52:19 +0200

    Update dependencies

--- src/Handler/ForgotPasswordHandler.php
+++ src/Handler/ForgotPasswordHandler.php
@@ -1,27 +1,43 @@
 <?php
 
-
 namespace App\Handler;
-
 
 use App\Form\ForgotPasswordType;
 use App\HandlerFactory\AbstractHandler;
+use App\Repository\UserRepository;
+use App\Service\SendMail;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class ForgotPasswordHandler extends AbstractHandler
 {
     /** @var EntityManagerInterface $entityManager */
     private $entityManager;
 
+    /** @var UserRepository $userRepository */
+    private $userRepository;
+
+    /** @var SendMail $sendMail */
+    private $sendMail;
+
+    /** @var TokenGeneratorInterface $tokenGenerator */
+    private $tokenGenerator;
+
     public function __construct(
         FormFactoryInterface $formFactory,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository,
+        SendMail $sendMail,
+        TokenGeneratorInterface $tokenGenerator
     )
     {
         parent::__construct($formFactory);
         $this->entityManager = $entityManager;
+        $this->userRepository = $userRepository;
+        $this->sendMail = $sendMail;
+        $this->tokenGenerator = $tokenGenerator;
     }
 
     protected function configure(OptionsResolver $resolver): void
