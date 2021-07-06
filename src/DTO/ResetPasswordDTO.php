d87d224382b6d14bbe52daa538dec7598c54c23f
ludovicjj <jahanlud@gmail.com>
2021-07-06 17:07:10 +0200

    Add constraints to password property

--- src/DTO/ResetPasswordDTO.php
+++ src/DTO/ResetPasswordDTO.php
@@ -2,10 +2,24 @@
 
 namespace App\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class ResetPasswordDTO
 {
     /**
      * @var string|null $password
+     *
+     * @Assert\NotBlank(
+     *     message="Le champs password ne peut être vide."
+     * )
+     * @Assert\Regex(
+     *     pattern="/^(?=.*?[A-Z])(?=.*?[0-9])(?=.*[-+_!@#$%^&*., ?]).{8,}$/",
+     *     message="Votre mot de passe doit contenir au minimum 8 caractères avec au moins une majuscule, un chiffre et un caractère spécial."
+     * )
+     * @Assert\Length(
+     *     max=255,
+     *     maxMessage="Votre mot de passe dépasse la limite de caractères accepté."
+     * )
      */
     private $password;
 
