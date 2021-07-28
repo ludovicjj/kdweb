2bbecb6ab18532b2643bfbcfce003c219cc7f00e
ludovicjj <jahanlud@gmail.com>
2021-07-28 17:32:18 +0200

    Get access_token from discord

--- src/Security/DiscordUserProvider.php
+++ src/Security/DiscordUserProvider.php
@@ -1,19 +1,30 @@
 <?php
 
-
 namespace App\Security;
 
-
+use App\Repository\UserRepository;
 use App\Service\PasswordGenerator;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DiscordUserProvider implements UserProviderInterface
 {
+    private const DISCORD_ACCESS_TOKEN_END_POINT = "https://discord.com/api/oauth2/token";
+    private const DISCORD_USER_DATA_END_POINT = "https://discordapp.com/api/users/@me";
+
     /** @var EventDispatcherInterface $eventDispatcher */
     private $eventDispatcher;
 
@@ -29,10 +40,18 @@ class DiscordUserProvider implements UserProviderInterface
     /** @var string $discordClientSecret */
     private $discordClientSecret;
 
+    /** @var UserRepository $userRepository */
+    private $userRepository;
+
+    /** @var UrlGeneratorInterface $urlGenerator */
+    private $urlGenerator;
+
     public function __construct(
         EventDispatcherInterface $eventDispatcher,
         HttpClientInterface $client,
         PasswordGenerator $passwordGenerator,
+        UserRepository $userRepository,
+        UrlGeneratorInterface $urlGenerator,
         string $discordClientId,
         string $discordClientSecret
     )
@@ -40,50 +59,82 @@ class DiscordUserProvider implements UserProviderInterface
         $this->eventDispatcher = $eventDispatcher;
         $this->client = $client;
         $this->passwordGenerator = $passwordGenerator;
+        $this->userRepository = $userRepository;
+        $this->urlGenerator = $urlGenerator;
         $this->discordClientId = $discordClientId;
         $this->discordClientSecret = $discordClientSecret;
     }
 
     /**
-     * Loads the user for the given username.
-     *
-     * This method must throw UsernameNotFoundException if the user is not
-     * found.
-     *
-     * @return UserInterface
-     *
-     * @throws UsernameNotFoundException if the user is not found
+     * @param string $code
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
      */
-    public function loadUserByUsername(string $username): UserInterface
+    public function loadUserFromDiscordOAuth(string $code)
+    {
+        $accessToken = $this->getAccessToken($code);
+    }
+
+    /**
+     * @param string $code
+     * @return string
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    private function getAccessToken(string $code): string
+    {
+        $url = $this->urlGenerator->generate("app_login",
+            [
+                "discord-oauth-provider" => 1
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $options = [
+            "headers" => [
+                "Accept" => "application/json",
+                "Content-Type" => "application/x-www-form-urlencoded"
+            ],
+            "body" => [
+                "client_id" => $this->discordClientId,
+                "client_secret" => $this->discordClientSecret,
+                "code" => $code,
+                "grant_type" => "authorization_code",
+                "redirect_uri" => $url,
+                "scope" => "identify email"
+            ]
+        ];
+
+        $response = $this->client->request("POST", self::DISCORD_ACCESS_TOKEN_END_POINT, $options);
+        $data = $response->toArray();
+
+        if (!array_key_exists("access_token", $data)) {
+            throw new ServiceUnavailableHttpException(null, "Authentication from discord failed");
+        }
+
+        return $data["access_token"];
+    }
+
+
+    public function refreshUser(UserInterface $user)
     {
 
     }
 
-    /**
-     * Refreshes the user.
-     *
-     * It is up to the implementation to decide if the user data should be
-     * totally reloaded (e.g. from the database), or if the UserInterface
-     * object can just be merged into some internal array of users / identity
-     * map.
-     *
-     * @return UserInterface
-     *
-     * @throws UnsupportedUserException  if the user is not supported
-     * @throws UsernameNotFoundException if the user is not found
-     */
-    public function refreshUser(UserInterface $user): UserInterface
+
+    public function supportsClass(string $class)
     {
 
     }
 
-    /**
-     * Whether this provider supports the given user class.
-     *
-     * @return bool
-     */
-    public function supportsClass(string $class): bool
+    public function loadUserByUsername(string $username)
     {
-
+        // TODO: Implement loadUserByUsername() method.
     }
 }
