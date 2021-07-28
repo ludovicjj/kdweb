af478045d11a3d169523306748993a1804f93f44
ludovicjj <jahanlud@gmail.com>
2021-07-28 18:36:58 +0200

    Update discord authenticator

--- src/Security/DiscordAuthenticator.php
+++ src/Security/DiscordAuthenticator.php
@@ -2,52 +2,112 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class DiscordAuthenticator extends AbstractGuardAuthenticator
 {
-    public function supports(Request $request)
+    /** @var CsrfTokenManagerInterface $csrfTokenManager */
+    private $csrfTokenManager;
+
+    /** @var UrlGeneratorInterface $urlGenerator */
+    private $urlGenerator;
+
+    /** @var DiscordUserProvider $discordUserProvider */
+    private $discordUserProvider;
+
+    public function __construct(
+        CsrfTokenManagerInterface $csrfTokenManager,
+        UrlGeneratorInterface $urlGenerator,
+        DiscordUserProvider $discordUserProvider
+    )
     {
-        // todo
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->urlGenerator = $urlGenerator;
+        $this->discordUserProvider = $discordUserProvider;
     }
 
-    public function getCredentials(Request $request)
+    public function supports(Request $request): bool
     {
-        // todo
+        return $request->query->has("discord-oauth-provider");
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider)
+    public function getCredentials(Request $request): array
     {
-        // todo
+        $state = $request->query->get("state");
+
+        if (!$state || !$this->csrfTokenManager->isTokenValid(new CsrfToken("mon-super-token", $state))) {
+            throw new InvalidCsrfTokenException();
+        }
+
+        return [
+            "code" => $request->query->get("code")
+        ];
     }
 
-    public function checkCredentials($credentials, UserInterface $user)
+    /**
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     * @return User|null
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider): ?User
     {
-        // todo
+        if ($credentials === null)
+        {
+            return null;
+        }
+
+        return $this->discordUserProvider->loadUserFromDiscordOAuth($credentials["code"]);
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    public function checkCredentials($credentials, UserInterface $user): bool
     {
-        // todo
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): JsonResponse
+    {
+        return new JsonResponse([
+            "message" => "authentification refusé"
+        ], Response::HTTP_UNAUTHORIZED);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        // todo
+        return new RedirectResponse($this->urlGenerator->generate("app_user_account_home"));
     }
 
-    public function start(Request $request, AuthenticationException $authException = null)
+    public function start(Request $request, AuthenticationException $authException = null): JsonResponse
     {
-        // todo
+        return new JsonResponse([
+            "message" => "authentification requise"
+        ], Response::HTTP_UNAUTHORIZED);
     }
 
-    public function supportsRememberMe()
+    public function supportsRememberMe(): bool
     {
-        // todo
+        return false;
     }
 }
