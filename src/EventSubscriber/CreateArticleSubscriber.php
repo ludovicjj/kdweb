290c0eae5dfa5367b177dc412a349deeb1a3c1fd
ludovicjj <jahanlud@gmail.com>
2021-08-31 13:08:47 +0200

    Update CreateArticleSubscriber: add an other submit btn if user has role ROLE_ADMIN

--- src/EventSubscriber/CreateArticleSubscriber.php
+++ src/EventSubscriber/CreateArticleSubscriber.php
@@ -4,7 +4,38 @@
 namespace App\EventSubscriber;
 
 
-class CreateArticleSubscriber
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class CreateArticleSubscriber implements EventSubscriberInterface
 {
 
+    public static function getSubscribedEvents()
+    {
+        return [
+            FormEvents::PRE_SET_DATA => "onPreSetData"
+        ];
+    }
+
+    public function onPreSetData(FormEvent $event): void
+    {
+        $form = $event->getForm();
+
+        if (!$form->getConfig()->hasOption("user_role")) {
+            return;
+        }
+
+        $role = $form->getConfig()->getOption("user_role");
+
+        if (in_array("ROLE_ADMIN", $role, true)) {
+            $form->add("save", SubmitType::class, [
+                "label" => "Sauvegarder cette article comme brouillon",
+                "attr" => [
+                    "class" => "btn btn-warning"
+                ]
+            ]);
+        }
+    }
 }
