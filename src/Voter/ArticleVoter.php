cee7a6fd22b1e93610e52dcb183fe3d242ee2a45
ludovicjj <jahanlud@gmail.com>
2021-09-07 18:02:51 +0200

    Update ArticleVoter: only users with role ROLE_WRITER and are author can create articles

--- src/Voter/ArticleVoter.php
+++ src/Voter/ArticleVoter.php
@@ -1,25 +1,34 @@
 <?php
 
-
 namespace App\Voter;
-
 
 use App\Entity\Article;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
 
 class ArticleVoter extends Voter
 {
+    private const CAN_CREATE_ROLE = "ROLE_WRITER";
+    public const CREATE = "create";
     public const EDIT = "edit";
+
+    /** @var RoleHierarchyInterface $roleHierarchy */
+    private $roleHierarchy;
+
+    public function __construct(RoleHierarchyInterface $roleHierarchy)
+    {
+        $this->roleHierarchy = $roleHierarchy;
+    }
 
     protected function supports(string $attribute, $subject): bool
     {
-        if (!in_array($attribute, [self::EDIT])) {
+        if (!in_array($attribute, [self::EDIT, self::CREATE])) {
             return false;
         }
 
-        if (!$subject instanceof Article) {
+        if ($subject !== null && !$subject instanceof Article) {
             return false;
         }
 
@@ -37,11 +46,31 @@ class ArticleVoter extends Voter
         $article = $subject;
 
         switch ($attribute) {
+            case self::CREATE:
+                return $this->canCreate($user);
             case self::EDIT:
                 return $this->canEdit($article, $user);
         }
 
         throw new \LogicException('This code should not be reached!');
+    }
+
+    public function canCreate(User $user): bool
+    {
+        if (
+            !in_array(
+            self::CAN_CREATE_ROLE,
+            $this->roleHierarchy->getReachableRoleNames($user->getRoles()),
+            true)
+        ) {
+            return false;
+        }
+
+        if ($user->getAuthor() === null) {
+            return false;
+        }
+
+        return true;
     }
 
     private function canEdit(Article $article, User $user): bool
